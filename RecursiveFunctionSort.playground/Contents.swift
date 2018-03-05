d79b07638aa0ec574370d15d0dbaac74745a8a44
dely2p <lhyal2p@gmail.com>
2018-03-05 16:23:13 +0900

    add Recursive Function Sort

---
+++ RecursiveFunctionSort.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Recursive Function Sort
+
+func run() {
+    let list = [5, 3, 9, 1, 7]
+    let result = recursiveSort(list: list, index: 0)
+    print(result)
+}
+
+func recursiveSort(list: [Int], index: Int) -> [Int] {
+    let index = index
+    var list = list
+    if index > list.count-1 {
+        return list
+    }else {
+        for itemIndex in index..<list.count {
+            if list[index] > list[itemIndex] {
+                list.swapAt(index, itemIndex)
+            }
+        }
+        list = recursiveSort(list: list, index: index+1)
+    }
+    return list
+}
+
+run()
